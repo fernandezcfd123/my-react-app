d934b4fad9929cfc5defc29640df8176bbcbcaad
fernandezcfd123 <fernandezcfd123@gmail.com>
2021-04-20 11:16:16 +0200

    ReactApp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p id="p1">¿Que quieres hacer?</p>
+        <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+        <button id="uno">Dar de alta PAS</button>
+        <button id="dos">Dar de alta Centro Sanitario</button>
+        <button id="tres">Dar de alta médico</button>
+        <button id="cuatro">Crear cuadro médico</button>
+        <button id="cinco">Visualizar Cuadros Médicos</button>
+        <button id="seis">Buscar, modificar y eliminar Cuadro Médico</button>
+        <button id="siete">Buscar Centro Sanitario</button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p id="uno">¿Que quieres hacer?</p>
-        <h1>My React App</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
